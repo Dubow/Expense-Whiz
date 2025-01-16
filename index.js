9c2b90d96653a5b55410024bf4687d788e318486
Abdirahman <abdirahmandubow4@gmail.com>
2025-01-16 23:36:41 +0800

    updating index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/Public/Expense/signup.html')
 })
 
+app.get('/signin', (req, res) => {
+    res.sendFile(__dirname + '/Public/Expense/signin.html')
+})
+
 const PORT = 3000
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}')
